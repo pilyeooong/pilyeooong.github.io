d889ca60f23515b43f4f46c3be513686b8255084
PilYeong <pilyeooong@gmail.com>
2019-10-16 14:19:15 +0900

    project섹션에 슬라이드추가

--- static/js/main.js
+++ static/js/main.js
@@ -18,7 +18,28 @@
         $('html, body').animate({scrollTop : offset.top}, 400);
     }
 // ---------------image sliding (header) ----------------------
+var slideIndex = 1;
+showSlides(slideIndex);
 
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("projectbox");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
 
 // var slideIndex = 0;
 // showSlides();
